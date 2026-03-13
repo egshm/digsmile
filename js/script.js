a914e39c52b1510b52be01b5732c6aaf3f9a8f5c
Shoma Eguchi <eguchi.engineer@gmail.com>
2026-03-13 11:37:53 +0900

    （＃2）パフォーマンス改善 - WebP画像出し分け処理を追加 - Lazy画像を導入 - Google Fonts和文フォントの使用を撤廃 - Google Fonts欧文フォントをセルフホスティング - css/jsファイルを圧縮して使用 - その他、フォント周りの軽微な表示不具合を修正

--- js/script.js
+++ js/script.js
@@ -67,45 +67,144 @@ function stickyHeader() {
 // スムーススクロール
 //--------------------------------------------
 
-// ページ内スムーススクロール
-document.querySelectorAll('a[href^="#"]:not([href^="#!"]').forEach(function(anchor) {
-  anchor.addEventListener("click", function(event) {
-    // デフォルトのイベント動作をキャンセル
-    event.preventDefault();
+// 全img要素の読込み完了を監視
+//--------------------------------------------
+function onAllImagesLoaded(callback) {
+  const images = document.querySelectorAll("img");
+  let loadedCount = 0;
+  let isCalled = false;
 
-    const id = anchor.getAttribute("href");
-    const target = document.querySelector(id === "#" || id === "" ? "html" : id);
+  // callback の重複実行を防ぐ
+  function done() {
+    if (!isCalled) {
+      isCalled = true;
+      callback();
+    }
+  }
 
-    if (target) {
-      const headerHeight = header.offsetHeight;
-      // ターゲット要素のドキュメント全体における絶対位置からヘッダー高さを引いた位置を算出
-      const position = target.getBoundingClientRect().top + window.pageYOffset - headerHeight;
-      // ターゲット位置までスムーススクロール
-      window.scrollTo({
-        top: position,
-        behavior: "smooth"
+  // 画像が無い場合は即実行
+  if (images.length === 0) {
+    done();
+    return;
+  }
+
+  images.forEach(image => {
+    // すでに読込みが完了している場合
+    if (image.complete) {
+      loadedCount++;
+      if (loadedCount === images.length) done();
+      return;
+    }
+
+    // 読込みが完了した場合
+    image.addEventListener('load', function() {
+      loadedCount++;
+      if (loadedCount === images.length) done();
+    });
+
+    // 読込みエラーの場合
+    image.addEventListener('error', function() {
+      loadedCount++;
+      if (loadedCount === images.length) done();
+    });
+  });
+}
+
+
+// スムーススクロール
+//--------------------------------------------
+
+// スクロールオフセット調整値（px）
+const SCROLL_OFFSET_ADJUST = 20;
+
+// ページ遷移後のスクロール待機時間（ms）
+const SCROLL_DELAY_TIME = 300;
+
+document.querySelectorAll('a[href*="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const href = anchor.getAttribute('href');
+
+    // ページ内スクロール
+    if (href.startsWith('#')) {
+
+      // 遅延読込み画像を強制的に読込むように設定
+      document.querySelectorAll('img[loading="lazy"]').forEach(lazyImage => {
+        lazyImage.loading = "eager";
       });
+
+      // 画像の読み込みがすべて完了したら処理を継続
+      onAllImagesLoaded(function() {
+
+        const target = document.querySelector(
+          href === '#' || href === '' ? 'html' : href
+        );
+        const offset = header.offsetHeight + SCROLL_OFFSET_ADJUST;
+        const position = target.getBoundingClientRect().top + window.pageYOffset;
+
+        // スムーススクロール
+        window.scrollTo({
+          top: position - offset,
+          behavior: 'smooth'
+        });
+
+        // URLにハッシュを追加
+        history.pushState(null, '', href);
+      });
+    }
+    // ページ外へ遷移
+    else {
+      // 移動先ページ
+      const destPage = anchor.href.split('#')[0];
+      // 移動先ハッシュ
+      const destHash = anchor.href.split('#')[1];
+
+      // ハッシュを一時保存
+      sessionStorage.setItem('scrollTarget', destHash);
+
+      // 移動先ページに遷移
+      window.location.href = destPage;
     }
   });
 });
 
 // ページ外スムーススクロール
-urlHash = window.location.hash;
-if (urlHash) {
-  history.replaceState(null, "", window.location.pathname);
-  window.scrollTo(0, 0);
-  setTimeout(function() {
-    const target = document.querySelector(urlHash);
+window.addEventListener('load', function() {
 
-    if (target) {
-      const headerHeight = header.offsetHeight;
-      // ターゲット要素のドキュメント全体における絶対位置からヘッダー高さを引いた位置を算出
-      const position = target.getBoundingClientRect().top + window.pageYOffset - headerHeight;
-      // ターゲット位置までスムーススクロール
-      window.scrollTo({
-        top: position,
-        behavior: "smooth"
-      });
+  // 遅延読込み画像を強制的に読込むように設定
+  document.querySelectorAll('img[loading="lazy"]').forEach(lazyImage => {
+    lazyImage.loading = "eager";
+  });
+
+  // 画像の読み込みがすべて完了したら処理を継続
+  onAllImagesLoaded(function() {
+
+    // スクロール先のハッシュ値を取得
+    const urlHash = sessionStorage.getItem('scrollTarget');
+
+    if (urlHash) {
+      // ページの先頭に移動
+      window.scrollTo(0, 0);
+      // 保存情報を破棄
+      sessionStorage.removeItem('scrollTarget');
+      // 移動先を取得
+      const target = document.querySelector('#' + urlHash);
+
+      if (target) {
+        const offset = header.offsetHeight + SCROLL_OFFSET_ADJUST;
+        const position = target.getBoundingClientRect().top + window.pageYOffset;
+
+        setTimeout(function(){
+          // 移動先へスムーススクロール
+          window.scrollTo({
+            top: position - offset,
+            behavior: 'smooth'
+          });
+          // ハッシュを再設定
+          history.replaceState(null, '', window.location.pathname + '#' + urlHash);
+        }, SCROLL_DELAY_TIME);
+      }
     }
-  }, 50);
-}
+  });
+});
